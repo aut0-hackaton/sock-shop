468202acfd62f0f8ea9ff453432ac3b8ba3f5d2a
Sergejs Solopovs <sergejs.solopovs@if.lv>
2019-10-08 10:30:25 +0300

    added dependencies initial project

---
+++ tests/Dependencies/WebShop.Api/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebShop.Api
+{
+    public class Class1
+    {
+    }
+}
---
+++ tests/Dependencies/WebShop.Ui/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WebShop.Ui
+{
+    public class Class1
+    {
+    }
+}
