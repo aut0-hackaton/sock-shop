b92e90077e73742cb7bf98f8e74bffb17edd3a99
Kaspars Kočāns <kaspars.kocans@if.lv>
2019-10-08 13:00:23 +0300

    added product card

--- tests/Dependencies/WebShop.Ui/PageObjects/Content/ProductCard.cs
+++ tests/Dependencies/WebShop.Ui/PageObjects/Content/ProductCard.cs
@@ -7,10 +7,19 @@ namespace WebShop.Ui.PageObjects.Content
 {
     public class ProductCard
     {
+        private readonly By price = By.ClassName("price");
+        private readonly By addToCartBtn = By.ClassName("button-primary");
         private IWebElement scope;
         public ProductCard(IWebElement scope)
         {
             this.scope = scope;
+        }
+
+        public string Price => scope.FindElement(price).Text;
+
+        public void AddToCart()
+        {
+            scope.FindElement(addToCartBtn).Click();
         }
     }
 }
