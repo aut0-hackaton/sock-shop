ded911db2acff7ede80e884171d73658dcd49a95
Kaspars Kočāns <kaspars.kocans@if.lv>
2019-10-08 13:59:41 +0300

    added hooks

---
+++ tests/WebShop.E2E.Tests/Hooks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace WebShop.E2E.Tests
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+        private IWebDriver webDriver;
+        private ScenarioContext scenarioContext;
+
+        public Hooks(ScenarioContext scenarioContext)
+        {
+            this.scenarioContext = scenarioContext;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            webDriver = CreateWebDriver();
+            webDriver.Manage().Window.Maximize();
+            scenarioContext.Set(webDriver);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            //TODO: implement logic that has to run after executing each scenario
+        }
+
+        public IWebDriver CreateWebDriver()
+        {
+            return new ChromeDriver($"{AppDomain.CurrentDomain.BaseDirectory}/chromedriver.exe");
+        }
+    }
+}
