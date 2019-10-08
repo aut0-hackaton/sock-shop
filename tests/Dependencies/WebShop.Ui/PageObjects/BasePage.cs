50155ca001243370dfa6672478b9eb6ee7dda42c
KKaspars <kaspars.kocans@gmail.com>
2019-10-08 11:30:55 +0300

    added login form

--- tests/Dependencies/WebShop.Ui/PageObjects/BasePage.cs
+++ tests/Dependencies/WebShop.Ui/PageObjects/BasePage.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Text;
 using OpenQA.Selenium;
+using WebShop.Ui.PageObjects.Content;
 
 namespace WebShop.Ui.PageObjects
 {
     public class BasePage
     {
-        private IWebDriver webDriver;
+        private readonly By login = By.Id("login");
+
+        protected IWebDriver webDriver;
+        protected IWebElement scope;
         public BasePage(IWebDriver webDriver)
         {
             this.webDriver = webDriver;
+        }
+
+        public BasePage(IWebDriver webDriver, By scope)
+        {
+            this.webDriver = webDriver;
+            this.scope = webDriver.FindElement(scope);
+        }
+
+        public LoginForm OpenLoginForm()
+        {
+            webDriver.FindElement(login).Click();
+            return new LoginForm(webDriver);
         }
     }
 }
---
+++ tests/Dependencies/WebShop.Ui/PageObjects/Content/LoginForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace WebShop.Ui.PageObjects.Content
+{
+    public class LoginForm : BasePage
+    {
+        private readonly By userName = By.Id("username-modal");
+        private readonly By password = By.Id("password-modal");
+        private readonly By loginBtn = By.CssSelector("button.btn-primary");
+
+        public LoginForm(IWebDriver webDriver) 
+            : base(webDriver, By.ClassName("modal-dialog"))
+        {
+        }
+
+        public string Username
+        {
+            get => scope.FindElement(userName).Text;
+            set => scope.FindElement(userName).SendKeys(value);
+        }
+
+        public string Password
+        {
+            get => scope.FindElement(password).Text;
+            set => scope.FindElement(password).SendKeys(value);
+        }
+
+        public BasePage Login()
+        {
+            scope.FindElement(loginBtn).Click();
+            return this;
+        }
+    }
+}
