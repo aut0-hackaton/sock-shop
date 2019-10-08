783955af5454f00645a7977fd38405a9cd89c944
Sergejs Solopovs <sergejs.solopovs@if.lv>
2019-10-08 10:25:37 +0300

    initial project structure

---
+++ tests/WebShop.Api.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebShop.Api.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ tests/WebShopt.E2E.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebShopt.E2E.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
