e5f96bb56072be5a9e83ff2ac67e4b4caceb38c8
KKaspars <kaspars.kocans@gmail.com>
2019-10-08 11:12:23 +0300

    added specflow and selenium packages and base page

---
+++ tests/Dependencies/WebShop.Ui/PageObjects/BasePage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace WebShop.Ui.PageObjects
+{
+    public class BasePage
+    {
+        private IWebDriver webDriver;
+        public BasePage(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+    }
+}
